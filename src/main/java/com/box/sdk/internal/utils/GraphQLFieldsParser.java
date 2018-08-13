3a0d2c7284973b1b5df28bb4406c5102329c3dba
carycheng <ccheng@box.com>
2018-08-13 12:44:51 -0700

    added parser for additional fields

--- src/main/java/com/box/sdk/internal/utils/GraphQLFieldsParser.java
+++ src/main/java/com/box/sdk/internal/utils/GraphQLFieldsParser.java
@@ -5,7 +5,13 @@ package com.box.sdk.internal.utils;
  */
 public class GraphQLFieldsParser {
 
-    public String fieldsParser() {
+    public String fieldsParser(String... fields) {
+        StringBuilder builder = new StringBuilder();
 
+        for (String field : fields) {
+            builder.append(field);
+        }
+
+        return builder.toString();
     }
 }
