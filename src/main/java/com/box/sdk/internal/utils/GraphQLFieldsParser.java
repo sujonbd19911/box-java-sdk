e2dc776f75fe068469090b5029a1871c62952a5f
carycheng <ccheng@box.com>
2018-08-14 10:36:49 -0700

    added space between fields in the graphql parser

--- src/main/java/com/box/sdk/internal/utils/GraphQLFieldsParser.java
+++ src/main/java/com/box/sdk/internal/utils/GraphQLFieldsParser.java
@@ -5,6 +5,12 @@ package com.box.sdk.internal.utils;
  */
 public class GraphQLFieldsParser {
 
+    /**
+     * This is a helper function to take in the optional fields and create a space separated string to pass into the
+     * workflow query string.
+     * @param fields    The fields to retrieve from the workflow query.
+     * @return  A space separated string for optional fields.
+     */
     public static String parseFields(String... fields) {
         StringBuilder builder = new StringBuilder();
 
